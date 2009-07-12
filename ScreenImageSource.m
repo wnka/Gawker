wnka/Gawker

commit 5683abade3356547833e7e68ee2dcbe6b94a75b8

Updated project to xcode 3.1 and osx 10.5 plus fixed a minor compile bug with this change

ScreenImageSource.m
@@ -183,8 +183,8 @@ static void swizzleBitmap(NSBitmapImageRep * bitmap);
     CGLPixelFormatAttribute attribs[] = {
         kCGLPFAFullScreen,
         kCGLPFADisplayMask,
-        displayMask,
-        nil
+        (CGLPixelFormatAttribute)0,
+		(CGLPixelFormatAttribute)0
     };
         
     float screenWidth = [screen frame].size.width;

Gawker.xcodeproj/project.pbxproj
@@ -3,7 +3,7 @@
 	archiveVersion = 1;
 	classes = {
 	};
-	objectVersion = 42;
+	objectVersion = 45;
 	objects = {
 
 /* Begin PBXBuildFile section */
@@ -551,7 +551,7 @@
 		29B97313FDCFA39411CA2CEA /* Project object */ = {
 			isa = PBXProject;
 			buildConfigurationList = ED5C98C208763692003C1476 /* Build configuration list for PBXProject "Gawker" */;
-			compatibilityVersion = "Xcode 2.4";
+			compatibilityVersion = "Xcode 3.1";
 			hasScannedForEncodings = 1;
 			mainGroup = 29B97314FDCFA39411CA2CEA /* Gawker */;
 			projectDirPath = "";
@@ -809,9 +809,10 @@
 		ED5C98C308763692003C1476 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
-				MACOSX_DEPLOYMENT_TARGET = 10.4;
+				ARCHS = "$(NATIVE_ARCH)";
+				MACOSX_DEPLOYMENT_TARGET = 10.5;
 				OTHER_LDFLAGS = "";
-				SDKROOT = /Developer/SDKs/MacOSX10.4u.sdk;
+				SDKROOT = macosx10.5;
 				ZERO_LINK = NO;
 			};
 			name = Debug;
@@ -819,13 +820,10 @@
 		ED5C98C408763692003C1476 /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
-				ARCHS = (
-					ppc,
-					i386,
-				);
-				MACOSX_DEPLOYMENT_TARGET = 10.4;
+				ARCHS = "$(NATIVE_ARCH)";
+				MACOSX_DEPLOYMENT_TARGET = 10.5;
 				OTHER_LDFLAGS = "";
-				SDKROOT = /Developer/SDKs/MacOSX10.4u.sdk;
+				SDKROOT = macosx10.5;
 				ZERO_LINK = NO;
 			};
 			name = Release;
@@ -833,9 +831,10 @@
 		ED5C98C508763692003C1476 /* Default */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
-				MACOSX_DEPLOYMENT_TARGET = 10.4;
+				ARCHS = "$(NATIVE_ARCH)";
+				MACOSX_DEPLOYMENT_TARGET = 10.5;
 				OTHER_LDFLAGS = "";
-				SDKROOT = /Developer/SDKs/MacOSX10.4u.sdk;
+				SDKROOT = macosx10.5;
 				ZERO_LINK = NO;
 			};
 			name = Default;

commit 1b3d4e425a7f99dc1c9857d9300fe2f767543c45

Fixed regression to ScreenImageCapture caused in commit 5683abade3356547833e7e68ee2dcbe6b94a75b8. Bumping version to v0.8.2 so we can issue an update.

Info.plist
@@ -17,7 +17,7 @@
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>0.8.1</string>
+	<string>0.8.2</string>
 	<key>CFBundleHelpBookFolder</key>
 	<string>Gawker Help</string>
 	<key>CFBundleHelpBookName</key>

ScreenImageSource.m
@@ -183,9 +183,9 @@ static void swizzleBitmap(NSBitmapImageRep * bitmap);
     CGLPixelFormatAttribute attribs[] = {
         kCGLPFAFullScreen,
         kCGLPFADisplayMask,
-        (CGLPixelFormatAttribute)0,
-		(CGLPixelFormatAttribute)0
-    };
+        displayMask,
+        (CGLPixelFormatAttribute)NULL
+	};
         
     float screenWidth = [screen frame].size.width;
     float screenHeight = [screen frame].size.height;